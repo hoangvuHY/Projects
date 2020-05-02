bcf1709920ced6bd7f654f7414c4e5ecd9c1e8ad
Vuoppa <vuoppa@example.net>
2020-05-02 22:17:31 +0700

    Xong ve feature va gallery trong bootslander

--- 1Bootslander/main.js
+++ 1Bootslander/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var bodyTop = $('body,html').scrollTop();
+    var bodyTop = $('body').scrollTop();
     $(window).scroll(function () {
         var navbarTop = $("#navbarTop").offset().top;
         if (navbarTop > bodyTop) {
@@ -8,5 +8,24 @@ $(document).ready(function () {
         } else {
             $("#navbarTop").removeClass('scrollDown');
         }
+    });
+    $("#navbarTop .nav-item  .nav-link").click(function (e) {
+        e.preventDefault();
+        e.preventDefault();
+        var href = $(this).attr("href");
+        var linkBootslander = $(href).offset().top;
+        console.log(linkBootslander);
+        $("html").animate({
+            scrollTop: linkBootslander - 60
+        }, 2000, "easeInOutExpo");
+    });
+
+    new WOW().init();
+
+
+    /* FancyBox */
+    $(".linkImageGallery").fancybox({
+        openEffect: 'easeInOutExpo',
+        closeEffect: 'easeInOutExpo',
     });
 });
